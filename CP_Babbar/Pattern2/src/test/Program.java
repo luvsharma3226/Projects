88dd16dfa58245c2af901cf549baacb6b9484d71
luvsharma <luvsharma0906@gmail.com>
2022-07-05 12:25:19 +0530

    testing from eclipse

--- CP_Babbar/Pattern2/src/test/Program.java
+++ CP_Babbar/Pattern2/src/test/Program.java
@@ -6,6 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n: ");
 		int n = sc.nextInt();
 		int i = 1;
 		while(i <= n) {
