6c0e72cd131f337bac4bda0fa28f4ccdd5261465
luvsharma <luvsharma0906@gmail.com>
2022-09-19 11:42:06 +0530

    Babbar

--- CP_Babbar/SortArray012/src/test/Program.java
+++ CP_Babbar/SortArray012/src/test/Program.java
@@ -11,17 +11,6 @@ public class Program {
 	public static void main(String[] args) {
 
 		int array[] = { 0, 2, 1, 2, 0, 1, 2 };
-		int start = 0, end = array.length - 1;
-
-		for (int i = 0; i < array.length; i++) {
-			for (int j = i; j < array.length; j++) {
-				if (array[i] > array[j]) {
-					int temp = array[i];
-					array[i] = array[j];
-					array[j] = temp;
-				}
-			}
-		}
 
 	}
 }
